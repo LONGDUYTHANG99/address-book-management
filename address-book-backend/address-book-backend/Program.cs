a828f2547e2ae361d6f02520779aa9c8a6c27569
LongDuyThang <longduythangcbvn@gmail.com>
2025-06-16 22:46:22 +0700

    Build Base project, add configurations

--- address-book-backend/address-book-backend/Program.cs
+++ address-book-backend/address-book-backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+
+    options.AddPolicy(name: "AnyOrigin",
+        cfg =>
+        {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+});
+
+//builder.Services.AddDbContext<ApplicationDBContext>(options =>
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("DefaultConnection"))
+//);
+
+
 
 var app = builder.Build();
 
@@ -16,10 +43,36 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//Configure Exception Handler
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+//Configure response caching
+app.MapGet("/error",
+    [EnableCors("AnyOrigin")]
+[ResponseCache(NoStore = true)] () =>
+        Results.Problem());
+
+app.MapGet("/error/test",
+    [EnableCors("AnyOrigin")]
+[ResponseCache(NoStore = true)] () =>
+    {
+        throw new Exception("test");
+    });
+
+
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers().RequireCors("AnyOrigin");
 
 app.Run();
--- address-book-backend/address-book-backend/obj/Debug/net8.0/address-book-backend.AssemblyInfo.cs
+++ address-book-backend/address-book-backend/obj/Debug/net8.0/address-book-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("address-book-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c41cfd4d9442bf777cdb0f178b0fb879548ec12")]
 [assembly: System.Reflection.AssemblyProductAttribute("address-book-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("address-book-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
